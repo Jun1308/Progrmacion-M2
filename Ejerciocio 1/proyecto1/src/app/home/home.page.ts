5b84548615916074e2c14eb09bdb7ff2c601f710
Jun1308 <gonzalezjuanluis084@gmail.com>
2026-06-26 19:17:35 -0400

    .

--- Ejerciocio 1/proyecto1/src/app/home/home.page.ts
+++ Ejerciocio 1/proyecto1/src/app/home/home.page.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { callOutline, personOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonButtons, IonMenuButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [IonButtons, IonMenuButton, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, CommonModule, FormsModule, RouterLink]
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ callOutline, personOutline });
+  }
 
   ngOnInit() {
   }
